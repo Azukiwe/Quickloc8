9f34e8147b68a1517e260ebd60db4ea2c3ba78f4
Azukiwe <dywiliuzukhanye@gmail.com>
2023-06-13 15:52:20 +0200

    trying

---
+++ src/components/MapVue.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="map"></div>
+</template>
+
+<script>
+// import L from 'leaflet';
+export default {
+    mounted() {
+//         let map = L.map('map').fitWorld();
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
+
+//     L.marker(e.latlng).addTo(map)
+//         .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+
+// map.on('locationfound', onLocationFound);
+// function onLocationError(e) {
+//     alert(e.message);
+// }
+
+// map.on('locationerror', onLocationError); 
+    }
+}
+</script>
+<style scoped>
+#map{
+    height: 100vh;
+    width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="map"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import L from 'leaflet';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    mounted() {
+        let map = L.map('map').setView([-33.918861, 18.423300], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+map.locate({setView: true, maxZoom: 16});
+//street map
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 16,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+googleStreets.addTo(map)
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
+
+//     L.marker(e.latlng).addTo(map)
+//         .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+let Marker = L.marker([-34.040278, 18.677778]);
+let Up = Marker.bindPopup('Your current location').openPopup()
+Up.addTo(map)
+
+map.on('locationfound', onLocationFound);
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError); 
+    }
 }
 </script>
+<style scoped>
+#map{
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    justify-content: center;
+}
+</style>
+
